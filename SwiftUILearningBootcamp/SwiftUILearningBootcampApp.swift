128bf4698a8cd90a74585c9dc911b513080f2e2f
Dimuthu Lakshan <dimuthu_l@epiclanka.net>
2024-07-08 11:51:43 +0530

    Initial Commit

---
+++ SwiftUILearningBootcamp/SwiftUILearningBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningBootcampApp.swift
+//  SwiftUILearningBootcamp
+//
+//  Created by Dimuthu Lakshan on 2024-07-08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
