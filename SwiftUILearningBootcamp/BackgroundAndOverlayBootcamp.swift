b96ba76e76bc1191a83f45e0d9829da092a7b78d
Dimuthu Lakshan <dimuthu_l@epiclanka.net>
2024-07-16 15:22:35 +0530

    ""

---
+++ SwiftUILearningBootcamp/BackgroundAndOverlayBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundAndOverlayBootcamp.swift
+//  SwiftUILearningBootcamp
+//
+//  Created by Dimuthu Lakshan on 2024-07-16.
+//
+
+import SwiftUI
+
+struct BackgroundAndOverlayBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundAndOverlayBootcamp()
+}
---
+++ SwiftUILearningBootcamp/FrameBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  FrameBootCamp.swift
+//  SwiftUILearningBootcamp
+//
+//  Created by Dimuthu Lakshan on 2024-07-16.
+//
+
+import SwiftUI
+
+struct FrameBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameBootCamp()
+}
